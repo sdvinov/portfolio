fda37000df54fc558fd0155ba61bc9d698c755cf
sdvinov <odvakr.wikia@gmail.com>
2017-05-08 04:33:11 -0400

    Created several pages

--- wp-content/themes/advertica-lite/SketchBoard/functions/admin-init.php
+++
@@ -1,7 +0,0 @@
-<?php
-/********************************************
- FRAME WORK CODE STARTS HERE
-*********************************************/
-require_once('sketch-functions.php');                // Pagination, excerpt control etc.
-require_once('sketch-enqueue.php');                  // Enqueue Css Scripts
-require_once('sketch-breadcrumb.php');               // custom post types includes
