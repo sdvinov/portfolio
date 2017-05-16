78b241393211e9333a776f721fae0f6401a01952
sdvinov <odvakr.wikia@gmail.com>
2017-05-16 01:00:05 -0400

    nav styling

--- wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
+++ wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -10,5 +10,16 @@
 
 ?>
 <div class="site-info">
-	<a href="https://wordpress.org/">Proudly powered by WordPress</a>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/angular/">Angular Example</a></li>
+		<li><a href="/api">API Parsing</a></li>
+		<li><a href="/arduino">Arduino Example</a></li>
+		<li><a href="/debug">Debug Tool</a></li>
+		<li><a href="/ecoenergo">Ecoenergo</a></li>
+		<li><a href="/regions">Regions App</a></li>
+		<li><a href="/rfc">RFC</a></li>
+		<li><a href="/shortme">URL-Shortener</a></li>
+		<li><a href="/yal">Youtube and Lyrics</a></li>
+	</ul>
 </div>
