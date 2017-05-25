5c5012742000abdfaf525cc00056438b8b81ee48
sdvinov <odvakr.wikia@gmail.com>
2017-05-25 14:04:27 -0400

    More styling

--- wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
+++ wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -11,15 +11,15 @@
 ?>
 <div class="site-info">
 	<ul>
-		<li><a href="/angular/"><img src="http://localhost/wp-content/uploads/2017/05/nglr-276x300.png" alt="" width="15" height="15" class="alignnone size-medium wp-image-210" /> Angular Example</a></li>
-		<li><a href="/api"><img src="http://localhost/wp-content/uploads/2017/05/jq-1-300x300.png" alt="" width="15" height="15" class="alignnone size-medium wp-image-211" /> API Parsing</a></li>
-		<li><a href="/arduino"><img src="http://localhost/wp-content/uploads/2017/05/rdino-300x300.png" alt="" width="15" height="15" class="alignnone size-medium wp-image-214" /> Arduino Example</a></li>
-		<li><a href="/debug"><img src="http://localhost/wp-content/uploads/2017/05/nodejs-300x300-1.png" alt="" width="15" height="15" class="alignnone size-full wp-image-216" /> Debug Tool</a></li>
-		<li><a href="/ecoenergo"><img src="http://localhost/wp-content/uploads/2017/05/html-219x300.png" alt="" width="15" height="15" class="alignnone size-medium wp-image-218" /> Ecoenergo</a></li>
-		<li><a href="/regions"><img src="http://localhost/wp-content/uploads/2017/05/rn.png" alt="" width="15" height="15" class="alignnone size-full wp-image-219" /> Regions App</a></li>
-		<li><a href="/rfc"><img src="http://localhost/wp-content/uploads/2017/05/html-219x300.png" alt="" width="15" height="15" class="alignnone size-medium wp-image-218" /> RFC</a></li>
-		<li><a href="/shortme"><img src="http://localhost/wp-content/uploads/2017/05/nodejs-300x300-1.png" alt="" width="15" height="15" class="alignnone size-full wp-image-216" /> URL-Shortener</a></li>
-		<li><a href="/yal"><img src="http://localhost/wp-content/uploads/2017/05/sf-300x300.png" alt="" width="15" height="15" class="alignnone size-medium wp-image-217" /> Youtube and Lyrics</a></li>
+		<li><a href="/angular/"><img src="http://localhost/wp-content/uploads/2017/05/nglr-1.png" alt="" width="15" height="15" class="alignnone size-medium wp-image-210" /> Angular Example</a></li>
+		<li><a href="/api"><img src="http://localhost/wp-content/uploads/2017/05/jq-2.png" alt="" width="15" height="15" class="alignnone size-medium wp-image-211" /> API Parsing</a></li>
+		<li><a href="/arduino"><img src="http://localhost/wp-content/uploads/2017/05/rdino-2.png" alt="" width="15" height="15" class="alignnone size-medium wp-image-214" /> Arduino Example</a></li>
+		<li><a href="/debug"><img src="http://localhost/wp-content/uploads/2017/05/nodejs-300x300-2.png" alt="" width="15" height="15" class="alignnone size-full wp-image-216" /> Debug Tool</a></li>
+		<li><a href="/ecoenergo"><img src="http://localhost/wp-content/uploads/2017/05/html-1.png" alt="" width="15" height="15" class="alignnone size-medium wp-image-218" /> Ecoenergo</a></li>
+		<li><a href="/regions"><img src="http://localhost/wp-content/uploads/2017/05/react.png" alt="" width="15" height="15" class="alignnone size-full wp-image-219" /> Regions App</a></li>
+		<li><a href="/rfc"><img src="http://localhost/wp-content/uploads/2017/05/html-1.png" alt="" width="15" height="15" class="alignnone size-medium wp-image-218" /> RFC</a></li>
+		<li><a href="/shortme"><img src="http://localhost/wp-content/uploads/2017/05/nodejs-300x300-2.png" alt="" width="15" height="15" class="alignnone size-full wp-image-216" /> URL-Shortener</a></li>
+		<li><a href="/yal"><img src="http://localhost/wp-content/uploads/2017/05/sf.png" alt="" width="15" height="15" class="alignnone size-medium wp-image-217" /> Youtube and Lyrics</a></li>
 	</ul>
 	<ul>
 		<li><a href="/"><img src="http://localhost/wp-content/uploads/2017/05/home-300x240.png" alt="" width="15" height="15" class="alignnone size-medium wp-image-220" /> Home</a></li>
